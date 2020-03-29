415914cfeea4ce2c454932e4cb931d838fee218c
Frank <62523949+FrankShao318@users.noreply.github.com>
2020-03-28 23:03:20 -0700

    Update menu.py

--- .nuke/menu.py
+++ .nuke/menu.py
@@ -1,11 +1,16 @@
 #----------------------------------------------------------------------------
 # menu.py
-# Version: 1.0.0
-# Last Updated: Mar. 22nd, 2020
+# Version: 1.0.1
+# Last Updated: Mar. 28, 2020
+#----------------------------------------------------------------------------
+
+#----------------------------------------------------------------------------
+# Global Imports ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 #----------------------------------------------------------------------------
 
 import nuke
 import platform
+import nukescripts
 
 # Define where .nuke directory is on each OS's network.
 
@@ -27,7 +32,49 @@ else:
 
 
 
+#----------------------------------------------------------------------------
+# Knob Defaults :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#----------------------------------------------------------------------------
+
+nuke.knobDefault("Tracker4.shutteroffset", "centered")
+nuke.knobDefault("Tracker4.label", "Motion: [value transform]\nRef Frame: [value reference_frame]")
+nuke.addOnUserCreate(lambda:nuke.thisNode()["reference_frame"].setValue(nuke.frame()), nodeClass="Tracker4")
+
+
+
+
+#----------------------------------------------------------------------------
+# Custom Menus ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#----------------------------------------------------------------------------
+
+frankMenu = nuke.menu("Nuke").addMenu("FrankMenu")
+frankMenu.addCommand("Autocrop", "nukescripts.autocrop()")
+
+
+frankGizmosMenu = nuke.menu("Nodes").addMenu("FrankGizmos", icon = dir + "/icons/frankGizmos.png")
+frankGizmosMenu.addCommand("Autocrop", "nukescripts.autocrop()")
+
+
+#----------------------------------------------------------------------------
 toolbar = nuke.menu("Nodes")
 toolbar.addCommand("FrankTool/GradeTool", "nuke.createNode('GradeTool')")
+
+
+
+#----------------------------------------------------------------------------
+# Keyboard Shortcuts ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#----------------------------------------------------------------------------
+
+nuke.menu("Nodes").addCommand("Transform/Tracker", "nuke.createNode('Tracker4')", "ctrl+alt+t", icon = "Tracker.png", shortcutContext = 2)
+
+
+
+
+
+
+
+
+
+
 
 
