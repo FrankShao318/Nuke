eade9427557f2843d1ddb6cef4286c2feda3e314
Frank <62523949+FrankShao318@users.noreply.github.com>
2020-04-10 22:55:47 -0700

    Update init.py

--- .nuke/init.py
+++ .nuke/init.py
@@ -1,13 +1,19 @@
 #----------------------------------------------------------------------------
 # init.py
-# Version: 1.0.0
-# Last Updated: Mar. 22nd, 2020
+# Version: 1.0.4
+# Last Updated: Apr. 10, 2020
 #----------------------------------------------------------------------------
 
 #-------Define Custom Folder Structure---------------------------------------
 
 
+nuke.pluginAddPath("./icons")
+
 nuke.pluginAddPath("./Gizmos")
+
+nuke.pluginAddPath("./Python")
 nuke.pluginAddPath("./Python/utilities_v01")
-nuke.pluginAddPath("ToolEngine")
 nuke.pluginAddPath("./Python/Ben_Python_Nuke_101")
+nuke.pluginAddPath("./Python/Ben_Python_Nuke_101/shuffleShortCuts")
+
+nuke.pluginAddPath("ToolEngine")
