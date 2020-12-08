d75d960cd1774f539035285a2e1aba6560e28803
Frank <62523949+FrankShao318@users.noreply.github.com>
2020-12-07 23:40:29 -0800

    Update init.py

--- .nuke/init.py
+++ .nuke/init.py
@@ -1,10 +1,10 @@
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 # init.py
-# Version: 1.0.4
-# Last Updated: Apr. 10, 2020
-#----------------------------------------------------------------------------
+# Version: 1.0.5
+# Last Updated: Dec. 7, 2020
+# ----------------------------------------------------------------------------
 
-#-------Define Custom Folder Structure---------------------------------------
+# -------Define Custom Folder Structure---------------------------------------
 
 
 nuke.pluginAddPath("./icons")
@@ -12,8 +12,14 @@ nuke.pluginAddPath("./icons")
 nuke.pluginAddPath("./Gizmos")
 
 nuke.pluginAddPath("./Python")
-nuke.pluginAddPath("./Python/utilities_v01")
+# nuke.pluginAddPath("./Python/utilities_v01")
 nuke.pluginAddPath("./Python/Ben_Python_Nuke_101")
 nuke.pluginAddPath("./Python/Ben_Python_Nuke_101/shuffleShortCuts")
 
-nuke.pluginAddPath("ToolEngine")
+# nuke.pluginAddPath("ToolEngine")
+
+nuke.pluginAddPath('./Python/Frank_Reel')
+nuke.pluginAddPath('./Python/Frank_Reel/nukeOutliner')
+nuke.pluginAddPath('./Python/Frank_Reel/nukeOutliner/ui')
+nuke.pluginAddPath('./Python/Pyside_for_nuke')
+nuke.pluginAddPath('./Python/Pyside_for_nuke/clipboard')
